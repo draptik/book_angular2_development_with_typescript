4c2941b27c19ac9792beb71f877eaf9fe2e6c80b
Patrick Drechsler <socialcoding@pdrechsler.de>
2018-01-19 10:20:06 +0100

    added product and basic home component setup

--- chapter02/auction/src/app/home/home.component.ts
+++ chapter02/auction/src/app/home/home.component.ts
@@ -1,13 +1,43 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/operator/delay';
+import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/finally';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/retryWhen';
+import 'rxjs/add/operator/take';
+
+import { ProductService} from '../shared/product.service';
+import { Product } from '../shared/product';
 
 @Component({
-  selector: 'app-home',
+  selector: 'app-home-page',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  products: Observable<Product[]>;
+  errorMessage: string;
+
+  constructor(private productService: ProductService) {
+    this.products = this.productService.getProducts();
+    //   .retryWhen(errors => {
+    //     this.errorMessage = `Please start the server. Retrying to connect.`;
+    //     return errors
+    //       .delay(2000) // Retry every 2 seconds
+    //       // take(3) // Max number of retries
+    //       .do(() => this.errorMessage += '.'); // Update the UI
+    //   })
+    //   .finally(() => this.errorMessage = null);
+    //
+    // this.productService.searchEvent
+    //   .subscribe(
+    //     params => this.products = this.productService.search(params),
+    //     error => console.error(error),
+    //     () => console.log('DONE')
+    //   );
+  }
 
   ngOnInit() {
   }
--- chapter02/auction/src/app/shared/product.service.ts
+++ chapter02/auction/src/app/shared/product.service.ts
@@ -5,4 +5,7 @@ export class ProductService {
 
   constructor() { }
 
+  getProducts() {
+
+  }
 }
